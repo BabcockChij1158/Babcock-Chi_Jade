69a93000b4400eaeccc1333b79e175eaab3e54f1
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-09-07 10:16:35 -0700

    Lab 02-Python

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,24 @@
+print("Multiplication:")
+
+a = 7
+b = 5
+area = a * b
+print("The number" , a, "multiplied by" , b, "is" , area)
+
+
+print("Concatenation:")
+
+Name = "Joe Silver"
+Adress = "12344 Plum Avenue,"
+City = " Capital City,"
+Zip = " 10034."
+
+print(Name, "lives in California; his address is" , (Adress + City + Zip))
+
+print("Complex Calculation:")
+l = 4
+w = 6
+h = 8
+side = 2 * (l * w + l * h + h * w)
+
+print("The surface area of your rectangular prism is" ,side)
