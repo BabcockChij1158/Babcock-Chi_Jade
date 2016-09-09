22c34667eacc3d30f67e50936497a163af47f9a5
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-09-09 10:26:10 -0700

    Lesson 3 files

---
+++ PyLesson_03/PyLab_03_BMICalc.py
@@ -0,0 +1,4 @@
+print("This is a BMI Calculator.")
+height = int (input("Please enter your height in inches: "))
+weight = int (input("Please enter your weight: "))
+print("Your BMI is " , (0.703*weight/height*height) , ".")
