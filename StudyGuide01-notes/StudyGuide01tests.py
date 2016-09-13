f76c3dcbe4fbcb312e6eb5e744b7f7bce190f82e
BabcockChij1158 <jadebc01@gmail.com>
2016-09-13 10:25:15 -0700

    added study guide

---
+++ StudyGuide01-notes/StudyGuide01tests.py
@@ -0,0 +1,23 @@
+num = 45
+print("Iron Man has" ,num, "kinds of weapons in his suit.")
+
+pres = "Abraham Lincoln"
+quote = "Whatever you are, be a good one."
+print(pres + " once said, \"" + quote + "\"")
+
+pres = "Abraham Lincoln"
+quote = "Whatever you are, be a good one."
+print(pres, "once said, \"",quote,"\"")
+
+h = 45
+w = 64
+print("The area of your rectangle is" ,h * w,".")
+print("The area of your rectangle is...", h * w)
+
+answer1 = input("How many siblings do you have? ")
+print("Wow, you have" ,answer1, "siblings.")
+print("This program works.")
+
+print("This one goes on top\nThis one goes on bottom")
+
+print("a\tb\tc\nd\te\tf\ng\th\ti")
