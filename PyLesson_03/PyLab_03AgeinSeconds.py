fb79eb5ea92f217a53a25174bc61d03f0f5f618e
BabcockChij1158 <jadebc01@gmail.com>
2016-09-16 09:46:00 -0700

    Added complete age in seconds file, weird stuff happ.

---
+++ PyLesson_03/PyLab_03AgeinSeconds.py
@@ -0,0 +1,4 @@
+print("This program calculates you age in seconds.")
+age = int (input("How old are you?"))
+name = input("What is your name?")
+print("Wow" , name, ", you are" , age*365*24*60*60 , "seconds old!")
