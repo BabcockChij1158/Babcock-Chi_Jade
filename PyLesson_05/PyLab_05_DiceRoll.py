59dac5ed8995c714e307406ea95ecbe42b89203c
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-10-07 10:25:57 -0700

    edited dice roll and reciept lab 05

---
+++ PyLesson_05/PyLab_05_DiceRoll.py
@@ -0,0 +1,19 @@
+print("This program simualates a dice roll, the winner is the one who rolled \nthe highest number between you and a computer.")
+
+import random
+num1 = random.randint(1,6)
+num2 = random.randint(1,6)
+
+def rolldice():
+    print("You rolled a",num1)
+    print("Computer rolled a",num2)
+
+rolldice()
+
+if num1 > num2:
+    print("The winner is you!!")
+
+if not num1 > num2:
+    print("The winner is computer!!")
+
+
---
+++ PyLesson_05/PyLab_05_Reciept.py
@@ -0,0 +1,41 @@
+def printf(item, price):
+    print("*{:>16} ........ {:>8.2f}".format(item, price))
+    
+item1 = input("Please enter item 1:")
+price1 = float(input("Please enter the price:"))
+item2 = input("Please enter item 2:")
+price2 = float(input("Please enter the price:"))
+item3 = input("Please enter item 3:")
+price3 = float(input("Please enter the price:"))
+item4 = input("Please enter item 4:")
+price4 = float(input("Please enter the price:"))
+global discount;
+
+subtotal = (price1+price2+price3+price4)
+tax = (subtotal*0.08)
+
+def calcdiscount():
+    if subtotal >= 2000:
+        discount = 0.15
+
+    if not subtotal >= 2000:
+        discount = 0.00
+
+total = subtotal - discount + tax
+
+
+print("<<<<<<<<<<<<<<<__Receipt__>>>>>>>>>>>>>>>")
+print("")
+printf(item1, price1)
+printf(item2, price2)
+printf(item3, price3)
+printf(item4, price4)
+print("")
+printf("Subtotal:", subtotal)
+printf("Tax:", tax)
+printf("Discount:", discount)
+printf("Total:", total)
+#same thing for tax, and total
+print("_________________________________________")
+#Print thank you message
+print("     * Thank you for your support *")
