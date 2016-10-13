f24152ec6c1c145668d0c97ad687a95ecf60a46c
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-10-13 10:24:34 -0700

    Started adventure lab 05.2

---
+++ PyLesson_05.2/PyLab_05.2_Adventure.py
@@ -0,0 +1,7 @@
+print("In the magical land of Panera, one warrior is on his way to defeat the evil bread monster. You are that warrior.")
+LeftOrRight = int(input("Would you like to..."+
+                        "\n 1. Take the path going to the left"+
+                        "\n 2. Take the path going to the right"))
+
+if LeftOrRight == 1:
+    left = int(input(""))
