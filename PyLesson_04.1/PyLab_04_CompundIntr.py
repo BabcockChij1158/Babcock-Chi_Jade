e64c37123e1e26f43223a60cb936708a68dc2388
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-10-20 10:25:43 -0700

    Edit comp intr 04.1 and add lab 06 files

--- PyLesson_04.1/PyLab_04_CompundIntr.py
+++ PyLesson_04.1/PyLab_04_CompundIntr.py
@@ -6,6 +6,6 @@ P = float(input("Enter your principal, the starting amount:"))
 r = float(input("Enter your interest rate in decimal:"))
 n = int(input("Enter the number of times the loan is compunded per year:"))
 t = int(input("Enter the life of the loan, in years:"))
-number = P*((1+r/n)**(n*t))
+number = (P*((1+r/n)**(n*t)))/(t*12)
 
-print("$",loan(number))
+print("You owe $",loan(number),"on your loan.")
