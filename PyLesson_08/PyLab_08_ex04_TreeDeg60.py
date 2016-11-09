6fd5b6fdd70627d294b1ba297cf765a2cedd9a4e
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-11-09 10:23:19 -0800

    finished lab 08

--- PyLesson_08/PyLab_08_ex04_TreeDeg60.py
+++ PyLesson_08/PyLab_08_ex04_TreeDeg60.py
@@ -1,8 +1,14 @@
 word = input("Enter in a word:")
 stop = len(word)
+start = 0
 
 def tree(word, start, stop):
     if start <= stop:
-        print
-        #FINISH Later
-        
+        print("{:>12}".format(word[0:start]))
+#I just set the print formatting to 12, but how do you set it to the length of the word?
+        start = start + 1
+        return tree(word, start, stop)
+    else:
+        return "ERROR"
+    
+tree(word, start, stop)
