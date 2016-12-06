c339f67b44d00795bcb90d16b38f1e85e4d34377
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2016-12-06 09:48:27 -0800

    expression solver might be fixed... not really sure

--- PyLesson_09/PyLab_09_ex06_ExpressionSolver.py
+++ PyLesson_09/PyLab_09_ex06_ExpressionSolver.py
@@ -1,15 +1,17 @@
 user = input("Enter in a mathematical expression such as 2 * 5...")
-equation = user.split(" ")
+equation = user.split()
 
 i = 0
 while i < len(equation):
     if i < len(equation) and equation[i] == "*" or "/":
         if equation[i] == "*":
             equation[i] = (int(equation[i-1]) * int(equation[i+1]))
-        else:
+            equation.pop(i+1)
+            equation.pop(i-1)
+        elif equation[i] == "/":
             equation[i] = (int(equation[i-1]) / int(equation[i+1]))
-        equation.remove[i-1]
-        equation.remove[i]
+            equation.pop(i+1)
+            equation.pop(i-1)
     i+=1
 
 i = 0
@@ -17,12 +19,14 @@ while i < len(equation):
     if i < len(equation) and equation[i] == "+" or "-":
         if equation[i] == "+":
             equation[i] = (int(equation[i-1]) + int(equation[i+1]))
-        else:
+            equation.pop(i+1)
+            equation.pop(i-1)
+        elif equation[i] == "-":
             equation[i] = (int(equation[i-1]) - int(equation[i+1]))
-        equation.remove[i-1]
-        equation.remove[i]
+            equation.pop(i+1)
+            equation.pop(i-1)
     i+=1
-
+#do you need recursion if the expression is more complex like: 4/5*53-23???
 print(equation)
 
     
