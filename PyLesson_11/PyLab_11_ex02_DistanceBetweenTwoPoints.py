9a4f742956d9f76a0cf6cf90b514376ec13f3817
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2017-01-09 10:25:25 -0800

    still doing lab 11

---
+++ PyLesson_11/PyLab_11_ex02_DistanceBetweenTwoPoints.py
@@ -0,0 +1,23 @@
+import math
+class Distance:
+    def __init__(self, x1, y1, x2, y2):
+        self.xOne = x1
+        self.yOne = y1
+        self.xTwo = x2
+        self.yTwo = y2
+        #double distance
+
+    def Resetx1(self, xone):
+        self.xOne = xone
+
+    def Resety1(self, yone):
+        self.yOne = yone
+
+    def Resetx2(self, xtwo):
+        self.xTwo = xtwo
+
+        self.yTwo = ytwo
+
+    def distance = Math.sqrt((xTwo-xOne)*(xTwo-xOne)+(yTwo-yOne)*(yTwo-yOne));
+
+        
