46ab4dfedf130da4f0355d92394bceafaec4e977
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2017-01-17 10:24:28 -0800

    almost done with 11.1

---
+++ PyLesson_11/PyLesson_11.1_ex01_CarClass.py
@@ -0,0 +1,45 @@
+##class Car:
+##    #Constructor
+##    def __init__(self, p, i, e, t):
+##        self.paint = p
+##        self.interior = i
+##        self.engine = e
+##        self.tires = t
+##
+##    #Modifier
+##    def setCustom(self, p, i, e, t):
+##        self.paint = p
+##        self.interior = i
+##        self.engine = e
+##        self.tires = t
+##
+##    #Accessors
+##    def getPaint(self):
+##        return.paint
+##
+##    def getInterior(self):
+##        return.interior
+##
+##    def getEngine(self):
+##        return.engine
+##
+##    def getTires(self):
+##        return.tires
+##
+##def main():
+##    paint = input("Enter the paint color:")
+##    interior = input("Enter the interior material:")
+##    engine = input("Enter the type of engine:")
+##    tires = input("Enter the type of tires:")
+##
+##    print("Your vehicle is ready...")
+##    car1 = Car(paint, interior, engine, tires)
+print("Paint:"+/t+"blue")
+
+
+
+
+
+
+
+#main()
