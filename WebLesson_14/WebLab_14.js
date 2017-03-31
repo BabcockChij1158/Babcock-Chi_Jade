017457659f85b1bb57b6c2d2104efc866848c16b
BabcockChij1158 <BabcockChiJ1158@sduhsd.lan>
2017-03-31 10:18:58 -0700

    i don't think lab 14 is done because it is glitchy

--- WebLesson_14/WebLab_14.js
+++ WebLesson_14/WebLab_14.js
@@ -1,11 +1,22 @@
 function validate(){
 	var x = document.forms.input.username.value;
-	alert(x);
+	var atPos = x.indexOf("@");
+	var dotPos = x.lastIndexOf(".");
+	var y = document.forms.input.pword.value;
+	
 //we need... username@webAddress.extension
 //if, the following...
-
-
-
-
-
+	//@ is not in the string
+	//@ is in the wrong place
+	//there is no .com, .gov, or any other extension
+	//final dot in the wrong place
+	
+	if(atPos < 1 || dotPos<atPos+2 || dotPos + 2 > x.length)
+		alert("This is not a valid email address!")
+	if(y.length < 6)
+		alert("Password must be at least 6 characters...")
+	if(atPos < 1 || dotPos < atPos+2 || dotPos + 2 > x.length && y.length < 6)
+		alert("Both username and password are invalid!")
+	else
+		alert("Success!")
 	}
