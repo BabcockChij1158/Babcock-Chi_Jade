924375aac3aacb004cb49b5e933739b1a615c97a
BabcockChij1158 <jadebc01@gmail.com>
2017-04-05 10:25:18 -0700

    my triangle won't show up >:(

--- WebLesson_15/WebLesson_15.js
+++ WebLesson_15/WebLesson_15.js
@@ -3,13 +3,15 @@ function shapes()
 	var x = document.getElementById("canvas"); //canvas is a container
 	//get allows us to use things that will enable to draw on the canvas
 	canvas = x.getContext("2d");
+	//canvas.strokeStyle = "yellow";
+	//canvas.fillStyle = "purple";
 	canvas.beginPath();
 	canvas.moveTo(50, 50);
 	canvas.lineTo(70, 250);
 	canvas.lineto(300, 200);
 	canvas.closePath();
 	canvas.stroke();
-	
+	//canvas.fill();
 	
 }
 
