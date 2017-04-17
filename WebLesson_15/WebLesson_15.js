2b0ef71a4007678907ab8cb31905b1d0c1e2f9e6
BabcockChij1158 <jadebc01@gmail.com>
2017-04-17 10:26:20 -0700

    just need to add gradient, to 15, but you said dont

--- WebLesson_15/WebLesson_15.js
+++ WebLesson_15/WebLesson_15.js
@@ -3,15 +3,31 @@ function shapes()
 	var x = document.getElementById("canvas"); //canvas is a container
 	//get allows us to use things that will enable to draw on the canvas
 	canvas = x.getContext("2d");
-	//canvas.strokeStyle = "yellow";
-	//canvas.fillStyle = "purple";
+	//var g = canvas.createLinearGradient();
+	//g.addColorStop(0, "purple");
+	//g.addColorStop(0.5, "white");
+	//g.addColorStop(1, "yellow");
+	//canvas.fillStyle = "g";
+	
 	canvas.beginPath();
-	canvas.moveTo(50, 50);
-	canvas.lineTo(70, 250);
-	canvas.lineto(300, 200);
+	canvas.moveTo(50, 200);
+	canvas.lineTo(150, 250);
+	canvas.lineTo(70, 350);
+	canvas.lineTo(200, 290);
+	canvas.lineTo(240, 400);
+	canvas.lineTo(270, 290);
+	canvas.lineTo(410, 350);
+	canvas.lineTo(330, 250);
+	canvas.lineTo(430, 200);
+	canvas.lineTo(315, 175);
+	canvas.lineTo(410, 90);
+	canvas.lineTo(270, 125);
+	canvas.lineTo(235, 15);
+	canvas.lineTo(200, 125);
+	canvas.lineTo(70, 90);
+	canvas.lineTo(165, 175);
 	canvas.closePath();
-	canvas.stroke();
-	//canvas.fill();
+	canvas.fill();
 	
 }
 
